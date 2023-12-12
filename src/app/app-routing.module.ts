c10701bfb5e9d1d5cadd25d91965282ecbd70826
caleb <bcaleb98@gmail.com>
2023-12-13 02:46:19 +0300

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ResetComponent } from './auth/reset/reset.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { FriendsComponent } from './friends/friends.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { path: 'reset', component: ResetComponent },
+  { path: 'forgot', component: ForgotComponent },
+  { path: 'newsfeed', component: NewsfeedComponent },
+  { path: 'friends', component: FriendsComponent },
+  {path: 'about',component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { ResetComponent } from './auth/reset/reset.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { FriendsComponent } from './friends/friends.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent,
+    ResetComponent,
+    NewsfeedComponent,
+    FriendsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
