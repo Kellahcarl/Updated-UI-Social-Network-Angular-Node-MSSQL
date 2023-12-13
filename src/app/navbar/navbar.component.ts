bcd1e04e911aa406cde05c4ac68be68e576fd252
caleb <bcaleb98@gmail.com>
2023-12-13 06:22:08 +0300

    reached somewhere

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  showFriendsModal = false;
+  showProfileModal = false;
+  showMobileMenu = false;
 
+  toggleFriendsModal() {
+    this.showFriendsModal = !this.showFriendsModal;
+  }
+
+  toggleProfileModal() {
+    this.showProfileModal = !this.showProfileModal;
+  }
+
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 }
